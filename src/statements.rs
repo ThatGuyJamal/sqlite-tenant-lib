4f3838859c2558f003ab4dbf1b019b543c72cbdb
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-04-25 14:34:17 -0400

    Update statements.rs

--- src/statements.rs
+++ src/statements.rs
@@ -1,9 +1,12 @@
 // https://www.sqlite.org/datatype3.html
 
+/// SQL statements used in the tenant manager.
 pub(crate) enum SqlStatement
 {
     CreateMasterDb,
     SelectTenantsOnLoad,
+    InsertAddTenant,
+    DeleteRemoveTenant,
 }
 
 impl SqlStatement
@@ -22,6 +25,11 @@ impl SqlStatement
                 )"
             }
             SqlStatement::SelectTenantsOnLoad => "SELECT id, tenant_path, tenant_has_path FROM tenants",
+            SqlStatement::InsertAddTenant => {
+                "INSERT INTO tenants (tenant_id, tenant_path, tenant_has_path, created_at) VALUES (?1, ?2, ?3, \
+                 CURRENT_TIMESTAMP)"
+            }
+            SqlStatement::DeleteRemoveTenant => "DELETE FROM tenants WHERE id = ?1",
         }
     }
 }
